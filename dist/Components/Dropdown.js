6e2e16875f126c04cbb6564ae07c86fdb194b89d
ludovic <ludovic.deckert@gmail.com>
2022-11-21 16:01:54 +0800

    npm changes

--- dist/Components/Dropdown.js
+++ dist/Components/Dropdown.js
@@ -5,9 +5,11 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = Dropdown;
 require("core-js/modules/web.dom-collections.iterator.js");
-var _react = require("react");
+var _react = _interopRequireWildcard(require("react"));
 var _arrow = _interopRequireDefault(require("./Images/arrow.png"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
+function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 function Dropdown(_ref) {
   let {
     label,
@@ -22,22 +24,22 @@ function Dropdown(_ref) {
   function selectedValue(e) {
     setSelected(e.target.innerText);
   }
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("label", {
+  return /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement("label", {
     className: "dropdownLabel"
-  }, " ", label, " "), /*#__PURE__*/React.createElement("button", {
+  }, " ", label, " "), /*#__PURE__*/_react.default.createElement("button", {
     className: "dropdownButton",
     onClick: handleOpen
-  }, /*#__PURE__*/React.createElement("p", null, selected), open ? /*#__PURE__*/React.createElement("img", {
+  }, /*#__PURE__*/_react.default.createElement("p", null, selected), open ? /*#__PURE__*/_react.default.createElement("img", {
     alt: "arrow up",
     className: "arrow active",
     src: _arrow.default
-  }) : /*#__PURE__*/React.createElement("img", {
+  }) : /*#__PURE__*/_react.default.createElement("img", {
     alt: "arrow down",
     className: "arrow",
     src: _arrow.default
-  })), open ? /*#__PURE__*/React.createElement("div", {
+  })), open ? /*#__PURE__*/_react.default.createElement("div", {
     className: "dropdownOptionsContainer"
-  }, /*#__PURE__*/React.createElement("ul", null, arr.map(item => /*#__PURE__*/React.createElement("li", {
+  }, /*#__PURE__*/_react.default.createElement("ul", null, arr.map(item => /*#__PURE__*/_react.default.createElement("li", {
     value: selected,
     className: "dropdownOption",
     key: item[field],
--- src/lib/Components/Dropdown.jsx
+++ src/lib/Components/Dropdown.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import arrow from "./Images/arrow.png";
 
 export default function Dropdown({ label, arr, field }) {
